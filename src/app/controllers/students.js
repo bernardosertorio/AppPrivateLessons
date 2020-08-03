0173e0b973a9af00cd6330a0473984598e633c31
Bernardo Sertório <besertorio@gmail.com>
2020-08-03 15:01:12 -0300

    :construction: Applying pagination to students

--- src/app/controllers/students.js
+++ src/app/controllers/students.js
@@ -6,10 +6,29 @@ module.exports = {
 
   list(req, res) {
 
-   Student.all(function(students) {
+    let { filter, page, limit } = req.query
 
-    return res.render("students/list", { students })
-   })
+    page = page || 1
+    limit = limit || 2
+    let offset = limit * (page - 1)
+
+    const params = {
+      filter,
+      page,
+      limit,
+      offseat,
+      callback(students) {
+
+        const pagination = {
+          total: Math.ceil(students[0].total / limit),
+          page
+        }
+        return res.render("students/list", { students, pagination, filter })
+      }
+    }
+
+    Student.paginate(params)
+   
 
   },
 
